f8b5a3b19ac37bccd11ab6ae04e1a2e0f4fb4a5b
unknown <UserXP@.(none)>
2011-09-28 14:36:40 +0300

    smarty

--- logic/test.php
+++ logic/test.php
@@ -1,7 +1,8 @@
 <?php
 include('../smarty/libs/Smarty.class.php');
+define ('BIBL_PATH', 'Z:/home/localhost/www/db/forHost');
 $smarty = new Smarty;
 $smarty->assign('name', 'george smith');
 $smarty->assign('address', '45th & Harris');
-$smarty->display('../templates/index.tpl');
+$smarty->display(BIBL_PATH . '/templates/index.tpl');
 ?>
