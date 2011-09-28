774ab605f4fb5d3c08bf78e72969940247c9c428
unknown <UserXP@.(none)>
2011-09-28 14:01:02 +0300

    smarty

--- logic/test.php
+++ logic/test.php
@@ -3,5 +3,5 @@ include('../smarty/libs/Smarty.class.php');
 $smarty = new Smarty;
 $smarty->assign('name', 'george smith');
 $smarty->assign('address', '45th & Harris');
-$smarty->display('../index.tpl');
+$smarty->display('../templates/index.tpl');
 ?>
