6cb067faf723778cc8124de620433dee3a09233c
unknown <UserXP@.(none)>
2011-09-28 13:56:58 +0300

    commit

--- logic/test.php
+++ logic/test.php
@@ -1,7 +1,7 @@
 <?php
-include('Smarty.class.php');
+include('../smarty/libs/Smarty.class.php');
 $smarty = new Smarty;
 $smarty->assign('name', 'george smith');
 $smarty->assign('address', '45th & Harris');
-$smarty->display('index.tpl');
+$smarty->display('../index.tpl');
 ?>
